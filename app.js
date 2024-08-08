8dfc657ec4a6e48eda9f49bcc6627827d65ad083
shashank00bhardwaj <98531138+shashank00bhardwaj@users.noreply.github.com>
2024-08-08 11:16:58 +0530

    08-08-2024

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const colleges = require('./dummy.js');
+const colleges = require('./public/dummy.js');
 
 
 app.use(express.static('public'));
