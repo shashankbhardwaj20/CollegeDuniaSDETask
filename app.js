8171bd12d50be58802e6efe8f89eaa1340f5f208
shashank00bhardwaj <98531138+shashank00bhardwaj@users.noreply.github.com>
2024-08-08 11:34:46 +0530

    08-08-2024

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
+const path =  require('path');
 const port = 3000;
 const colleges = require('./public/dummy.js');
 
 
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/colleges', (req, res) => {
     const { sort, order, search } = req.query;
